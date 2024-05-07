68d65e635ede647ebf2222c8df93b37a8464379d
Abhishekgupta011 <ag25061999@gmail.com>
2024-05-07 12:18:23 +0530

    linking

--- src/Pages/aboutus/[team].js
+++ src/Pages/aboutus/[team].js
@@ -1,11 +1,7 @@
 import { useRouter } from 'next/router';
+import { teamDetails } from './teamDetails';
 
-// Hardcoded list of team members
-const teamDetails = [
-  { id: '1', name: 'Yash', role: 'Senior Developer' },
-  { id: '2', name: 'Vaibhav', role: 'Backend Developer' },
-  { id: '3', name: 'Suresh', role: 'Frontend Developer' }
-];
+
 
 const TeamMemberDetails = ()=> {
   const router = useRouter();
--- src/Pages/aboutus/index.js
+++ src/Pages/aboutus/index.js
@@ -1,9 +1,20 @@
-const aboutPage = () =>{
+import Link from "next/link";
+import { teamDetails } from "./teamDetails";
+
+const AboutPage = () =>{
     return(
         <div>
             <h1>about Page</h1>
-
+            <ul>
+            {teamDetails.map((details)=>(
+                <li key={details.id}>
+                <Link href={`/aboutus/${details.id}`}>
+                    {details.name}
+                </Link>
+                </li>
+            ))}
+            </ul>
         </div>
     )
 }
-export default aboutPage;
+export default AboutPage;
